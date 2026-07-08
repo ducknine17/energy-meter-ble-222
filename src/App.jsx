7a015a2cb312435aba6234ccaaea145663f75f8a
ducknine17 <dazero1070@naver.com>
2026-07-08 16:22:10 +0900

    Add BLE device connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from "react";
 
-const [device, setDevice] = useState(null);
-const connectBluetooth = async () => {
-    try {
-        const bluetoothDevice = await navigator.bluetooth.requestDevice({
-            filters: [
-                { name: "Energy Meter" }
-            ],
-            optionalServices: [
-                "5b6b5f91-89a6-4dc8-b6d0-8f2c7a001001"
-            ]
-        });
-
-        setDevice(bluetoothDevice);
-
-        alert("Connected!");
-    }
-    catch (err)
-    {
-        console.error(err);
-    }
-};
-
 function App() {
+  const [device, setDevice] = useState(null);
+
+  const connectBluetooth = async () => {
+    try {
+      const bluetoothDevice = await navigator.bluetooth.requestDevice({
+        filters: [
+          { name: "Energy Meter" }
+        ],
+        optionalServices: [
+          "5b6b5f91-89a6-4dc8-b6d0-8f2c7a001001"
+        ]
+      });
+
+      setDevice(bluetoothDevice);
+
+      alert("Connected!");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
       <h1>Energy Meter</h1>
 
       <button onClick={connectBluetooth}>
-          Connect
+        Connect
       </button>
 
       <br /><br />
