28ba1c2cfa66335044b89e01db84106568bffde4
ducknine17 <dazero1070@naver.com>
2026-07-08 18:46:40 +0900

    Implement BLE command transmission

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [device, setDevice] = useState(null);
+  const [characteristic, setCharacteristic] = useState(null);
 
   const connectBluetooth = async () => {
     try {
@@ -14,12 +15,39 @@ function App() {
         ]
       });
 
+      const server = await bluetoothDevice.gatt.connect();
+      const service = await server.getPrimaryService(
+          "5b6b5f91-89a6-4dc8-b6d0-8f2c7a001001"
+      );
+      const characteristic = await service.getCharacteristic(
+          "5b6b5f91-89a6-4dc8-b6d0-8f2c7a001002"
+      );
       setDevice(bluetoothDevice);
-
+      setCharacteristic(characteristic);
       alert("Connected!");
     } catch (err) {
       console.error(err);
     }
+  };
+
+  const sendCommand = async (command) => {
+      if (!characteristic) {
+          alert("먼저 Connect를 눌러주세요.");
+          return;
+      }
+
+      try {
+          const encoder = new TextEncoder();
+
+          await characteristic.writeValue(
+              encoder.encode(command)
+          );
+
+          console.log("Sent :", command);
+
+      } catch (err) {
+          console.error(err);
+      }
   };
 
   return (
@@ -32,9 +60,17 @@ function App() {
 
       <br /><br />
 
-      <button>START</button>
-      <button>LAP</button>
-      <button>STOP</button>
+      <button onClick={() => sendCommand("START")}>
+          START
+      </button>
+
+      <button onClick={() => sendCommand("LAP")}>
+          LAP
+      </button>
+
+      <button onClick={() => sendCommand("STOP")}>
+          STOP
+      </button>
     </div>
   );
 }
